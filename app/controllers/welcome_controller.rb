8c4d24a1b262db840fe2e1a0b906de8f3364e9a8
Ari Drookman <aga.drookman@gmail.com>
2020-07-29 17:40:23 -0400

    Got the form to display depending on user being logged in. Lists down suggested trip locations.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
     @destination = Destination.new
+    # This is a list of 15 cities for suggestions on where to travel to.
+    @cities = ["London, England", "Bangkok, Thailand", "Paris, France", "Dubai, United Arab Emirates", "Istanbul, Turkey", "New York City, United States", "Singapore City, Singapore", "Kuala Lumpur, Malaysia", "Seoul, South Korea", "Hong Kong, China", "Tokyo, Japan", "Barcelona, Spain", "Amsterdam, Netherlands", "Rome, Italy", "Milan, Italy"]
   end
 
   def new
