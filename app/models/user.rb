bf12deb33a850d583db86f50456254de0b0e0701
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2020-07-28 20:39:08 -0400

    registration form with controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ApplicationRecord
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:login]
     has_many :trips
     has_many :reviews
     has_one :meal_preference
+
 end
