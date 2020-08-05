bb9eaaef38be7c595773aee9d0d6dc08f2ff62c9
Tal Sagi <Tal@Eusers-MacBook-Pro.local>
2020-08-04 22:23:00 -0400

    registration form with dropdown menus

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,22 @@
 class WelcomeController < ApplicationController
   def index
+    @users = User.all
+  end
+
+  def get_territories
+    @country = params[:country]
+    @territories = CS.get(@country)
+    respond_to do |format| 
+      format.json {render json: @territories}
+    end
+  end
+
+  def get_cities
+    @country = params[:country]
+    @territory = params[:territory]
+    @cities = CS.get(@country, @territory)
+    respond_to do |format|
+      format.json {render json: @cities}
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root :to => "welcome#index"
   devise_for :users, :controllers => {registrations: 'users/registrations', sessions: 'users/sessions'}
+  get '/users/get_territories/:country', to: 'welcome#get_territories' 
+  get '/users/get_cities/:country/:territory', to: 'welcome#get_cities' 
 end
